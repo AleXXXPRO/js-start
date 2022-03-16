d987c39ad74760ac9ce1f2a17a2f5635624e1a4c
Aleksandr Plastun <sunrise291178@gmail.com>
2022-03-16 11:25:11 +0200

    фрилансер по жизни

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict'
+// console.log("Hello world!");
+// console.log('Учу JS');
+// let myLifeStyle = 'freelance';
+// let myName = 'Фрилансер по жизни', myAge = 43, myMessage = 'Живи, а работай в свободное время';
+// myAge = 33;
+// console.log(myAge);
+// const BLOCK_SIZE = 25;
+// const COLOR_RED = '#F00';
+// const userProfile = {
+//     name: 'Фрилансер',
+//     age: 43,
+//     massage: 'Живи...',
+// }
+// console.log(userProfile);
+// userProfile.age = 33;
+// console.log(userProfile);
+// let colorEye = 'Цвет глаз';
+// let user ;
+// let userName = 'Вася';
+// user = userName;
+// console.log(user);
+
